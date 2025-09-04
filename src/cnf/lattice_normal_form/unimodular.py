3f4dd75cf1288866edf0accdfd264dde844c0b55
Max Gallant <mcgallant72@gmail.com>
2025-09-04 16:58:34 -0700

    Unimodular matrix helpers and tests

---
+++ src/cnf/lattice_normal_form/unimodular.py
@@ -0,0 +1,51 @@
+import numpy as np
+from enum import Enum
+
+class VoronoiVectors(Enum):
+
+    V0 = 0
+    V1 = 1
+    V2 = 2
+    V3 = 3
+    V0_V1 = 4
+    V0_V2 = 5
+    V0_V3 = 6
+
+
+COLUMNS = {
+    VoronoiVectors.V1: [1, 0, 0],
+    VoronoiVectors.V2: [0, 1, 0],
+    VoronoiVectors.V3: [0, 0, 1],
+
+    VoronoiVectors.V0: [-1, -1, -1],
+
+    VoronoiVectors.V0_V1: [0, -1, -1],
+    VoronoiVectors.V0_V2: [-1, 0, -1],
+    VoronoiVectors.V0_V3: [-1, -1, 0]
+}
+
+idx_to_voronoi_vec = { v.value: v for v in VoronoiVectors}
+voronoi_vec_to_idx = { v: v.value for v in VoronoiVectors}
+
+def get_unimodular_matrix_from_voronoi_vectors(vectors: list[VoronoiVectors]):
+    if not all([isinstance(v, VoronoiVectors) for v in vectors]):
+        raise ValueError("Enum values VoronoiVectors required!")
+    
+    cols = [COLUMNS[v] for v in vectors]
+    return np.array(cols).T
+
+def get_unimodular_matrix_from_voronoi_vector_idxs(v_idxs: list[int]):
+    vectors = [idx_to_voronoi_vec[i] for i in v_idxs]
+    return get_unimodular_matrix_from_voronoi_vectors(vectors)
+
+def get_unimodular_matrix_for_swap(vonorm_idx1, vonorm_idx2):
+    # vectors v1,v2,and v3 are our lattice generators
+    # v0 (the first one) forms the superbasis and v0=-v1-v2-v3
+    labels = [0,1,2,3,4,5,6]
+    tmp = labels[vonorm_idx2]
+    labels[vonorm_idx2] = labels[vonorm_idx1]
+    labels[vonorm_idx1] = tmp
+
+    # Now, in this new permutation, positions 1..3 hold
+    # the labels that correspond to the new vectors
+    return get_unimodular_matrix_from_voronoi_vector_idxs(labels[1:4])
---
+++ tests/lattice_normal_form/test_unimodular.py
@@ -0,0 +1,22 @@
+import cnf.lattice_normal_form.unimodular as uni
+import numpy as np
+
+def test_get_unimodular_matrix_from_voronoi_vector_idxs():
+    idxs = [2,1,3]
+    mat = uni.get_unimodular_matrix_from_voronoi_vector_idxs(idxs)
+    assert (mat.T[0] == [0,1,0]).all()
+    assert (mat.T[1] == [1,0,0]).all()
+    assert (mat.T[2] == [0,0,1]).all()
+
+
+    idxs = [0,3,2]
+    mat = uni.get_unimodular_matrix_from_voronoi_vector_idxs(idxs)
+    assert (mat.T[0] == [-1,-1,-1]).all()
+    assert (mat.T[1] == [0,0,1]).all()
+    assert (mat.T[2] == [0,1,0]).all()
+
+    idxs = [0,3,2]
+    mat = uni.get_unimodular_matrix_from_voronoi_vector_idxs(idxs)
+    assert (mat.T[0] == [-1,-1,-1]).all()
+    assert (mat.T[1] == [0,0,1]).all()
+    assert (mat.T[2] == [0,1,0]).all()
