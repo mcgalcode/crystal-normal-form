f5d14ec713232977037845bbad50e48de0dabf84
Max Gallant <mcgallant72@gmail.com>
2025-09-15 12:13:18 -0700

    Move basis normal form code to motif module

--- src/cnf/lattice/atomic_motif.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-class AtomicMotif():
-
-    def __init__(self, relative_coords: list[list[int | float]]):
-        self.relative_coords = np.array(relative_coords)
-    
-    def transform(self, transform: np.array):
-        self.relative_coords = self.relative_coords @ transform
--- src/cnf/motif/atomic_motif.py
+++ src/cnf/motif/atomic_motif.py
@@ -1,7 +1,9 @@
 import numpy as np
 from .utils import shift_coords, discretize_coords, sort_elements, sort_number_lists
 
-class ElementPositionMap():
+from ..lattice import Superbasis
+
+class AtomicMotif():
 
     
     @classmethod
@@ -23,13 +25,10 @@ class ElementPositionMap():
     
     def get_element_positions(self, el: str):
         if el not in self.map:
-            raise ValueError(f"Element {el} not found in ElementPositionMap")
+            raise ValueError(f"Element {el} not found in AtomicMotif")
         
         return self.map[el]
-    
-    def get_discretized_positions(self, el: str, delta: int):
-        unrounded = self.get_element_positions(el)
-        return [discretize_coords(c, delta) for c in unrounded]
+
 
     def unique_elements(self):
         return list(self.map.keys())
@@ -45,13 +44,10 @@ class ElementPositionMap():
         
         return elements, positions
 
-    def __len__(self):
-        return sum([len(positions) for _, positions in self.map.items()])
-    
-    def __repr__(self):
-        return self.map.__repr__()
-    
     def shift_origin(self, shift_vector: np.array):
+        if not self.is_valid_shift_vector(shift_vector):
+            raise ValueError(f"Tried to shift origin in {self.__class__} using invalid shift vector: {shift_vector}")
+        
         shift_vector = np.array(shift_vector)
         
         new_map = {}
@@ -61,8 +57,31 @@ class ElementPositionMap():
             for pos in pos_list:
                 new_map[el].append(shift_coords(pos, shift_vector))
         
-        return ElementPositionMap(new_map)
+        return AtomicMotif(new_map)
+
+    def is_valid_shift_vector(self, shift_vector: np.array):
+        return True
     
+    def __len__(self):
+        return sum([len(positions) for _, positions in self.map.items()])
+    
+    def __repr__(self):
+        return self.map.__repr__()
+    
+
+class FractionalMotif():
+
+    def get_discretized_positions(self, el: str, delta: int):
+        unrounded = self.get_element_positions(el)
+        return [discretize_coords(c, delta) for c in unrounded]
+    
+    def transform(self, transform: np.array):
+        transformed_coords = self.relative_coords @ transform
+        return FractionalMotif(transformed_coords, self.atom_species)
+    
+    def compute_cartesian_coords_in_basis(self, basis: Superbasis):
+        return CartesianMotif(self.relative_coords @ basis.generating_vecs())
+
     def get_sorted_discretized_positions(self, delta, element_order = None):
         if element_order is None:
             element_order = self.sorted_elements
@@ -80,3 +99,12 @@ class ElementPositionMap():
 
         discretized_positions_list = self.get_sorted_discretized_positions(delta, element_order)
         return [el for coord_list in discretized_positions_list for el in coord_list]
+
+    def is_valid_shift_vector(self, shift_vector: np.array):
+        return np.all(shift_vector >= 0 and shift_vector < 1.0)
+
+class CartesianMotif(AtomicMotif):
+    
+    pass
+    
+    
