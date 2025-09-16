e16b87d08d7600be07ecb480fc2ff6b64a93e2f5
Max Gallant <mcgallant72@gmail.com>
2025-09-16 10:01:30 -0700

    Add super helpful class for working with conorm lists

---
+++ src/cnf/lattice/conorm_list.py
@@ -0,0 +1,73 @@
+import enum
+from .permutations import CONORM_PERMUTATION_TO_VONORM_PERMUTATION, ConormPermutation, VonormPermutation
+
+class Conorms(enum.Enum):
+
+    P_01 = 0
+    P_02 = 1
+    P_03 = 2
+    P_12 = 3
+    P_13 = 4
+    P_23 = 5
+
+conorm_swaps = {
+    (0, 1): [(Conorms.P_02, Conorms.P_12), (Conorms.P_03, Conorms.P_13)],
+    (0, 2): [(Conorms.P_01, Conorms.P_12), (Conorms.P_03, Conorms.P_23)],
+    (0, 3): [(Conorms.P_02, Conorms.P_23), (Conorms.P_01, Conorms.P_13)],
+    (1, 2): [(Conorms.P_01, Conorms.P_02), (Conorms.P_13, Conorms.P_23)],
+    (1, 3): [(Conorms.P_01, Conorms.P_03), (Conorms.P_12, Conorms.P_23)],
+    (2, 3): [(Conorms.P_02, Conorms.P_03), (Conorms.P_12, Conorms.P_13)],
+}
+
+CONORM_INDICES_TO_PAIRS = {
+    0: {0,1},
+    1: {0,2},
+    2: {0,3},
+    3: {1,2},
+    4: {1,3},
+    5: {2,3}
+}
+
+class ConormList():
+
+    def __init__(self, conorms):
+        self.conorms = conorms
+        self.zero_indices = [idx for idx, conorm in enumerate(self.conorms) if conorm == 0]
+        self.voronoi_class = self._compute_voronoi_class()
+        self.permissible_permutations = self._compute_permissible_permutations()
+
+    def _compute_voronoi_class(self):
+        if len(self.zero_indices) == 3:
+            return 5
+        
+        if len(self.zero_indices) == 1:
+            return 2
+        
+        if len(self.zero_indices) == 0:
+            return 1
+        
+        if len(self.zero_indices) == 2:
+            pair_1 = self.zero_indices[0]
+            pair_2 = self.zero_indices[1]
+            if len(CONORM_INDICES_TO_PAIRS[pair_1].intersection(CONORM_INDICES_TO_PAIRS[pair_2])) == 0:
+                return 3
+            else:
+                return 4
+    
+    def is_permutation_permissible(self, permutation):
+        return permutation[-1] in self.zero_indices or permutation[-1] == 6
+        # vperm = ConormPermutation(permutation).to_vonorm_permutation()
+        # return 4 not in vperm[:4] and 5 not in vperm[:4] and 6 not in vperm[:4]
+    
+    def _compute_permissible_permutations(self) -> list[ConormPermutation]:
+        permissible_perms = []
+        for p in CONORM_PERMUTATION_TO_VONORM_PERMUTATION:
+            if self.is_permutation_permissible(p):
+                permissible_perms.append(ConormPermutation(p))
+        return permissible_perms
+
+    def __iter__(self):
+        return iter(self.conorms)
+ 
+    def __getitem__(self, key):
+        return self.conorms[key]
---
+++ tests/lattice/test_conorms.py
@@ -0,0 +1,66 @@
+from cnf.lattice.superbasis import Superbasis
+from cnf.lattice.unimodular import UnimodularMatrix
+from pymatgen.core.lattice import Lattice
+
+def test_v5_case():
+    cuboid = Lattice.cubic(1.0)
+    sb = Superbasis.from_pymatgen_lattice(cuboid)
+    conorms = sb.compute_vonorms().conorms
+    assert len(conorms.zero_indices) == 3
+    assert conorms.voronoi_class == 5
+    print(f"{conorms.voronoi_class}: {len(conorms.permissible_permutations)}")
+
+def test_v4_case():
+    hexagonal_prism = Lattice.hexagonal(1.0, 2.0)
+    sb = Superbasis.from_pymatgen_lattice(hexagonal_prism)
+    conorms = sb.compute_vonorms().conorms
+    assert len(conorms.zero_indices) == 2
+    assert conorms.voronoi_class == 4
+    print(f"{conorms.voronoi_class}: {len(conorms.permissible_permutations)}")
+
+def test_v3_case():
+    rhombic_dodecahedron = Lattice([
+        [1, 1, 0],
+        [1, -1, 0],
+        [-1, 0, 1],
+    ])
+    sb = Superbasis.from_pymatgen_lattice(rhombic_dodecahedron)
+    conorms = sb.compute_vonorms().conorms
+    assert len(conorms.zero_indices) == 2
+    assert conorms.voronoi_class == 3
+    print(f"{conorms.voronoi_class}: {len(conorms.permissible_permutations)}")
+
+def test_v2_case():
+    hexarhombic_dodecahedron = Lattice([
+        [1, 1, 0],
+        [1, -1, 0],
+        [-1, 0.2, 1],
+    ])
+    sb = Superbasis.from_pymatgen_lattice(hexarhombic_dodecahedron)
+    conorms = sb.compute_vonorms().conorms
+    assert len(conorms.zero_indices) == 1
+    assert conorms.voronoi_class == 2
+    print(f"{conorms.voronoi_class}: {len(conorms.permissible_permutations)}")
+    
+
+def test_v1_case():
+    truncated_octahedron = Lattice([
+        [1, 1, -1],
+        [1, -1, 1],
+        [-1, 1, 1],
+    ])
+    sb = Superbasis.from_pymatgen_lattice(truncated_octahedron)
+    conorms = sb.compute_vonorms().conorms
+    assert len(conorms.zero_indices) == 0
+    assert conorms.voronoi_class == 1
+    print(f"{conorms.voronoi_class}: {len(conorms.permissible_permutations)}")
+
+    unimodular_matrices = []
+    for p in conorms.permissible_permutations:
+        unimodular_matrices.append(UnimodularMatrix(p.to_unimodular_matrix()))
+    
+    mat_tuples = [m.tuple for m in unimodular_matrices]
+    print(f"{len(conorms.permissible_permutations)} distinct permutations")
+    print(f"{len(set(mat_tuples))} distinct unimodular matrices")
+    for m in unimodular_matrices:
+        print(m.matrix)
