b8bc86a9ba805d7a220457dc2741be146f6a6587
Max Gallant <mcgallant72@gmail.com>
2025-09-17 23:18:25 -0700

    Simplify rounding loop and make change selection more deterministic

--- src/cnf/lattice/rounding.py
+++ src/cnf/lattice/rounding.py
@@ -31,7 +31,6 @@ class DiscretizedVonormComputer():
         # The vonorm list comes in the following order:
         #
         # (v0^2, v1^2, v2^2, v3^2, (v0 + v1)^2, (v0 + v2)^2, (v0 + v3)^2)
-
         rounded_vonorms = np.round(np.array(self.true_vonorms) / self.lattice_step_size).astype(np.int64)
         primary_sum = np.sum(rounded_vonorms[:4])
         secondary_sum = np.sum(rounded_vonorms[4:])
@@ -42,38 +41,25 @@ class DiscretizedVonormComputer():
             # vonorms the least
             possible_changes = []
 
-            if primary_sum > secondary_sum:
-                # In this case, we have to either reduce a primary vonorm
-                # or increase a secondary vonorm
-                for idx in PRIMARY_VONORM_IDXS:
-                    adjustment = DECREMENT
-                    error_change = self.compute_error_change(rounded_vonorms, idx, adjustment)
-                    possible_changes.append((idx, adjustment, error_change))
+            for idx in PRIMARY_VONORM_IDXS + SECONDARY_VONORM_IDXS:
+                if primary_sum > secondary_sum:
+                    # In this case, we have to either reduce a primary vonorm
+                    # or increase a secondary vonorm
+                    adjustment = DECREMENT if idx in PRIMARY_VONORM_IDXS else INCREMENT
+                else:
+                    # In this case, we have to either INCREASE a PRIMARY vonorm
+                    # or DECREASE a SECONDARY vonorm
+                    adjustment = INCREMENT if idx in PRIMARY_VONORM_IDXS else DECREMENT
+                error_change = self.compute_error_change(rounded_vonorms, idx, adjustment)
+                possible_changes.append((error_change, idx, adjustment))
 
-                for idx in SECONDARY_VONORM_IDXS:
-                    adjustment = INCREMENT
-                    error_change = self.compute_error_change(rounded_vonorms, idx, adjustment)
-                    possible_changes.append((idx, adjustment, error_change))
-                    
-            else:
-                # In this case, we have to either INCREASE a PRIMARY vonorm
-                # or DECREASE a SECONDARY vonorm
-                for idx in PRIMARY_VONORM_IDXS:
-                    adjustment = INCREMENT
-                    error_change = self.compute_error_change(rounded_vonorms, idx, adjustment)
-                    possible_changes.append((idx, adjustment, error_change))
-
-                for idx in SECONDARY_VONORM_IDXS:
-                    adjustment = DECREMENT
-                    error_change = self.compute_error_change(rounded_vonorms, idx, adjustment)
-                    possible_changes.append((idx, adjustment, error_change))
-            
-            least_damaging_change = sorted(possible_changes, key=lambda c: c[2])[0]
-            idx_to_adjust, adjustment, _ = least_damaging_change
+            least_damaging_change = sorted(possible_changes)[0]
+            print(least_damaging_change)
+            _, idx_to_adjust, adjustment = least_damaging_change
             rounded_vonorms[idx_to_adjust] = rounded_vonorms[idx_to_adjust] + adjustment
             primary_sum = np.sum(rounded_vonorms[:4])
             secondary_sum = np.sum(rounded_vonorms[4:])
         
-        return rounded_vonorms
+        return [int(vo) for vo in rounded_vonorms]
 
         
