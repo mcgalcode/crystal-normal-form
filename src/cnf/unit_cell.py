8a9363b5967adfa575a429816e59e3c170f287ba
Max Gallant <mcgallant72@gmail.com>
2025-09-18 15:45:05 -0700

    Use new SublatticeGenerator class for unit cell supercells

--- src/cnf/unit_cell.py
+++ src/cnf/unit_cell.py
@@ -3,7 +3,7 @@ from pymatgen.core.structure import Structure
 from .lattice import Superbasis
 from .motif import FractionalMotif
 from .sublattice.gamma_matrices import GammaMatrixGroup
-from .sublattice.generation import transform_lattice_vecs, MotifTranslationSet
+from .sublattice.sublattice_generator import SublatticeGenerator
 
 class UnitCell():
 
@@ -18,12 +18,8 @@ class UnitCell():
         self.motif = motif
     
     def supercells(self, index: int):
-        gamma_matrices = GammaMatrixGroup.for_index(index)
-        supercells = []
-        for gm in gamma_matrices.matrices:
-            new_lattice = transform_lattice_vecs(self.superbasis.generating_vecs(), gm)
-            translators = MotifTranslationSet.from_gamma_matrix(gm)
-            new_motif = translators.apply_to_motif(self.motif)
-            supercells.append(UnitCell(Superbasis.from_generating_vecs(new_lattice), new_motif))
-        return supercells
+        sg = SublatticeGenerator.for_index(index)
+        lattice_vec_sets = sg.generate_sublattice_vector_sets(self.superbasis.generating_vecs())
+        motifs = sg.generate_sublattice_motifs(self.motif)
+        return [UnitCell(Superbasis.from_generating_vecs(lvs), motif) for lvs, motif in zip(lattice_vec_sets, motifs)]
 
