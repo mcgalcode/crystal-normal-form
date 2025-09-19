ee4cfcd25979670ff72cdc059dabfb9f8ee628b4
Max Gallant <mcgallant72@gmail.com>
2025-09-19 14:14:05 -0700

    Add helper class for Selling transform matrices

---
+++ src/cnf/lattice/selling/selling_transform_matrix.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+from ...linalg.matrix_tuple import MatrixTuple
+from .selling_pair import SellingPair
+
+LABELS_TO_COLS = {
+    1: np.array([1, 0, 0]),
+    2: np.array([0, 1, 0]),
+    3: np.array([0, 0, 1]),
+    0: np.array([-1, -1, -1]),
+}
+
+class SellingTransformMatrix(MatrixTuple):
+
+    @staticmethod
+    def from_pair(pair: SellingPair):
+        return SELLING_TRANSFORM_MATRICES[pair]
+    
+    @staticmethod
+    def inverse_from_pair(pair: SellingPair):
+        return SELLING_TRANSFORM_INVERSE_MATRICES[pair]
+
+def build_st_mat_from_pair(pair: SellingPair):
+    columns = []
+    i, j = pair
+    # We only care about vectors 1,2, and 3
+    for lattice_vec_label in range(1,4):
+        if lattice_vec_label == i:
+            col = - LABELS_TO_COLS[lattice_vec_label]
+        elif lattice_vec_label == j:
+            col = LABELS_TO_COLS[lattice_vec_label]
+        else:
+            col = LABELS_TO_COLS[lattice_vec_label] + LABELS_TO_COLS[i]
+        columns.append(col)
+    return SellingTransformMatrix(np.array(columns).T)
+
+SELLING_TRANSFORM_MATRICES: dict[SellingPair, SellingTransformMatrix] = {}
+SELLING_TRANSFORM_INVERSE_MATRICES: dict[SellingPair, SellingTransformMatrix] = {}
+
+for p in SellingPair.CANONICAL_PAIRS:
+    SELLING_TRANSFORM_MATRICES[p] = build_st_mat_from_pair(p)
+
+for p in SellingPair.CANONICAL_PAIRS:
+    SELLING_TRANSFORM_INVERSE_MATRICES[p] = SELLING_TRANSFORM_MATRICES[p].inverse()
---
+++ tests/lattice/selling/test_selling_transform_matrix.py
@@ -0,0 +1,22 @@
+import numpy as np
+from cnf.lattice.selling import SellingTransformMatrix
+
+def test_transform_matrices():
+    # I just computed these two with pen and paper
+    expected_mat = np.array([
+        [1,-1,-1],
+        [0,0,-1],
+        [0,-1,0],
+    ])
+    constructed_mat = SellingTransformMatrix.from_pair((0,1))
+
+    assert np.all(expected_mat == constructed_mat.matrix)
+
+    expected_mat_2 = np.array([
+        [-1,1,0],
+        [0,1,0],
+        [0,0,1],
+    ])
+    constructed_mat_2 = SellingTransformMatrix.from_pair((1,3))
+
+    assert np.all(expected_mat_2 == constructed_mat_2.matrix)
