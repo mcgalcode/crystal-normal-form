2412cf29137a4b9cc09417faa8f37c3d5cb81158
Max Gallant <mcgallant72@gmail.com>
2025-09-19 14:36:04 -0700

    Make SellingReducer abstract and fix bug

--- src/cnf/lattice/selling/selling_reducer.py
+++ src/cnf/lattice/selling/selling_reducer.py
@@ -4,18 +4,24 @@ from .selling_transform_matrix import SellingTransformMatrix
 from ..superbasis import Superbasis
 from ..vonorm_list import VonormList
 
-class SellingReducer():
+from abc import ABC, abstractmethod
+
+class SellingReducer(ABC):
 
     def __init__(self, tol = 0, verbose_logging = False, max_steps = 500):
         self.tol = tol
         self.max_steps = max_steps
         self._verbose_logging = verbose_logging
 
+    @abstractmethod
+    def apply_selling_transform(self, obj: Superbasis):
+        pass
+
     def reduce(self, object: VonormList | Superbasis):
         num_steps = 0
         transform_matrices = [np.eye(3)]
         while not object.is_obtuse(tol=self.tol):
-            object, acute_pair = object.selling_transform()
+            object, acute_pair = self.apply_selling_transform(object)
             if self._verbose_logging:
                 print(f"Selling transform {acute_pair}: {object}")
 
