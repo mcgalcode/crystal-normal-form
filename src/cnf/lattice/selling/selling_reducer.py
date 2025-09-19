187b4f796fb398b37fe7e0d9d65cb3d4d79c5940
Max Gallant <mcgallant72@gmail.com>
2025-09-19 14:26:42 -0700

    Add attempt at parent class for selling reduction

---
+++ src/cnf/lattice/selling/selling_reducer.py
@@ -0,0 +1,30 @@
+import numpy as np
+from .selling_reduction_result import SellingReductionResult
+from .selling_transform_matrix import SellingTransformMatrix
+from ..superbasis import Superbasis
+from ..vonorm_list import VonormList
+
+class SellingReducer():
+
+    def __init__(self, tol = 0, verbose_logging = False, max_steps = 500):
+        self.tol = tol
+        self.max_steps = max_steps
+        self._verbose_logging = verbose_logging
+
+    def reduce(self, object: VonormList | Superbasis):
+        num_steps = 0
+        transform_matrices = [np.eye(3)]
+        while not object.is_obtuse(tol=self.tol):
+            object, acute_pair = object.selling_transform()
+            if self._verbose_logging:
+                print(f"Selling transform {acute_pair}: {object}")
+
+            transform_matrices.append(SellingTransformMatrix.from_pair(acute_pair))
+            num_steps += 1
+            if num_steps > self.max_steps:
+                raise RuntimeError(f"Selling reduction failed to converge after {self.max_steps} steps")
+            
+        return SellingReductionResult(object,
+                                      transform_matrices=transform_matrices,
+                                      num_steps=num_steps,
+                                      tol=self.tol)
