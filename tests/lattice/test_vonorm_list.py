b0741d8ee8e251f2320e38e4cc7fd1201e5164f8
Max Gallant <mcgallant72@gmail.com>
2025-09-19 15:35:37 -0700

    rm unneeded import

--- tests/lattice/test_vonorm_list.py
+++ tests/lattice/test_vonorm_list.py
@@ -2,7 +2,6 @@ import pytest
 import numpy as np
 
 from cnf.lattice import Superbasis, VonormList
-from cnf.lattice.utils import selling_reduce
 from cnf.lattice.lattice_normal_form import LatticeNormalForm
 from pymatgen.core.lattice import Lattice
 
