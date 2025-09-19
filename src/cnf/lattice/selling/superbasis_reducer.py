1d70401f3d5e2c988533c7b4b276e76f2b7dff82
Max Gallant <mcgallant72@gmail.com>
2025-09-19 14:27:02 -0700

    Add superbasis reducer and first tests

---
+++ src/cnf/lattice/selling/superbasis_reducer.py
@@ -0,0 +1,80 @@
+import numpy as np
+
+from .selling_reducer import SellingReducer
+from ..superbasis import Superbasis
+
+def find_first_acute_pair(vecs: np.array):
+    """Given a list of vectors (perhaps an obtuse superbasis), iterates through
+    the pairwise dot products and if a positive dot product is encountered (a
+    pair of vectors separated by an acute angle), the indices of those vectors
+    are returned as a tuple, otherwise None is returned.
+
+    Parameters
+    ----------
+    vecs : np.array
+        The vectors to search for positive dot products between
+
+    Returns
+    -------
+    Union[None | Tuple[int, int]]
+        None if all angles are obtuses, or the pair of indices of vectors
+        which have an acute angle between them.
+    """
+    for i in range(len(vecs)):
+        for j in range(i + 1, len(vecs)):
+            if np.dot(vecs[i], vecs[j]) > 1e-7: # Use a tolerance
+                return (i, j)
+    return None
+
+class SuperbasisSellingReducer(SellingReducer):
+    
+    def apply_selling_transform(self, obj: Superbasis):
+        """Performs the Selling reduction step described in Lemma A.1 (reduction)
+        in Kurlin, 2022 "A complete isometry classification of 3-dimensional
+        lattices". You can only apply this function after you have identified a pair
+        of vectors in your superbasis whose dot product is positive (i.e. there is
+        an acute angle between them).
+
+        Note: This transformation is also defined in David Mrdjenovich's thesis
+        in Proof. [12] on page #18. It shows the matrix form of the reduction step
+        update as well.
+
+        Parameters
+        ----------
+        superbasis_vectors : np.array
+            The four non-obtuse superbasis vectors in need of reduction (v_i..l in
+            the paper)
+        first_acute_idx : int
+            The index of the first vector in the pair with a positive dot product.
+        second_acute_idx : int
+            The index of the second vector in the pair with a positive dot product.
+
+        Returns
+        -------
+        np.array
+            A new matrix where the rows are the new superbasis vectors
+            after the reduction step has been applied one time.
+        """        
+        acute_pair = find_first_acute_pair(obj.superbasis_vecs)
+        if acute_pair is None:
+            return self, None
+        first_acute_idx, second_acute_idx = acute_pair
+        transformed_vecs = self.get_transformed_vecs(obj.superbasis_vecs, first_acute_idx, second_acute_idx)
+        
+        return Superbasis(transformed_vecs), acute_pair
+    
+    def get_transformed_vecs(self, vecs, i, j):
+        transformed_vecs = np.zeros(vecs.shape)
+
+        # First assign the easy ones:
+        transformed_vecs[i] = -vecs[i]
+        transformed_vecs[j] = vecs[j]
+
+        # Now do the other two. The labeling (k, l in Kurlin) is arbitrary
+        # and they are both transformed the same way: by adding v_i to their
+        # previous values
+        other_idxs = {0, 1, 2, 3} - {i, j}
+        for other_idx in other_idxs:
+            transformed_vecs[other_idx] = vecs[other_idx] + vecs[i]        
+        return transformed_vecs
+
--- tests/lattice/selling/test_superbasis_reducer.py
+++ tests/lattice/selling/test_superbasis_reducer.py
@@ -1,6 +1,7 @@
 import pytest
-import cnf.lattice.selling as selling
 from pymatgen.core.structure import Structure
+
+from cnf.lattice.selling.superbasis_reducer import SuperbasisSellingReducer, find_first_acute_pair
 
 import numpy as np
 
@@ -33,7 +34,7 @@ def test_find_acute_pair():
         [0,1,0.5]
     ])
 
-    acute_pair = selling.find_first_acute_pair(lattice)
+    acute_pair = find_first_acute_pair(lattice)
     assert acute_pair[0] == 0
     assert acute_pair[1] == 1
 
@@ -44,7 +45,7 @@ def test_find_acute_pair():
         [0,1,0.5]
     ])
 
-    acute_pair = selling.find_first_acute_pair(lattice)
+    acute_pair = find_first_acute_pair(lattice)
     assert acute_pair[0] == 1
     assert acute_pair[1] == 3
 
@@ -55,7 +56,8 @@ def test_selling_transformation():
         [0,0,0.5],
         [0,1,0.5]
     ])
-    transformed = selling.apply_selling_transformation(lattice, 0, 1)
+    reducer = SuperbasisSellingReducer()
+    transformed = reducer.get_transformed_vecs(lattice, 0, 1)
 
     assert np.all(transformed[0] == [-1, 0, 0])
     assert np.all(transformed[1] == [1, 1, 0])
@@ -63,18 +65,9 @@ def test_selling_transformation():
     assert np.all(transformed[3] == [1, 1, 0.5])
 
     # Apply on indices 2 and 3
-    transformed2 = selling.apply_selling_transformation(transformed, 2, 3)
+    transformed2 = reducer.get_transformed_vecs(transformed, 2, 3)
 
     assert np.all(transformed2[0] == [0, 0, 0.5])
     assert np.all(transformed2[1] == [2, 1, 0.5])
     assert np.all(transformed2[2] == [-1, 0, -0.5])
     assert np.all(transformed2[3] == [1, 1, 0.5])
-
-def test_get_v0():
-    lattice = np.array([
-        [1, 0, 0],
-        [1, 1, 0],
-        [0,0,0.5]
-    ])
-    v0 = selling.get_v0_from_generating_vecs(lattice)
-    assert np.all(v0 == np.array([-2, -1, -0.5]))
