00604af8a7c27d56861505ebebd7a46935f8272f
Max Gallant <mcgallant72@gmail.com>
2025-09-23 12:40:41 -0700

    Add helper classes for voronoi values

---
+++ src/cnf/lattice/voronoi/voronoi_values.py
@@ -0,0 +1,89 @@
+from .vector_pair import VoronoiVectorPair
+
+class VoronoiValue():
+    pass
+
+
+CONORM_PAIRS_TO_IDXS = {
+    (0,1): 0,
+    (0,2): 1,
+    (0,3): 2,
+    (1,2): 3,
+    (1,3): 4,
+    (2,3): 5
+}
+class Conorm(VoronoiValue):
+
+    def __init__(self, pair: VoronoiVectorPair):
+        if not isinstance(pair, VoronoiVectorPair): 
+            pair = VoronoiVectorPair(*pair)        
+        self.pair = pair
+
+    @property
+    def i(self):
+        return self.pair[0]
+    
+    @property
+    def j(self):
+        return self.pair[1]
+    
+    def idx(self):
+        return CONORM_PAIRS_TO_IDXS[self.pair]
+    
+    def __repr__(self):
+        return f"Conorm(P_{self.i},{self.j})"
+    
+    def __eq__(self, other: 'SecondaryVonorm'):
+        return self.pair == other.pair
+
+class PrimaryVonorm(VoronoiValue):
+    
+    def __init__(self, vector_idx: int):
+        if vector_idx not in range(0, 4):
+            raise ValueError(f"PrimaryVonorm got invalid index: {vector_idx}")
+        self.idx = vector_idx
+    
+    def __eq__(self, other: 'PrimaryVonorm'):
+        return self.idx == other.idx
+
+    def __repr__(self):
+        return f"(V_{self.idx})^2"
+    
+class SecondaryVonorm(VoronoiValue):
+
+    def __init__(self, pair: VoronoiVectorPair):
+        if not isinstance(pair, VoronoiVectorPair):
+            pair = VoronoiVectorPair(*pair)
+        self.pair = pair
+    
+    @property
+    def complement(self):
+        all_idxs = set(range(0,4))
+        complement = all_idxs - set(self.pair)
+        return SecondaryVonorm(VoronoiVectorPair(*complement))
+    
+    @property
+    def is_canonical(self):
+        return 0 in self.pair
+    
+    def __repr__(self):
+        if self.is_canonical():
+            return f"(V_{self.pair[0]} + V_{self.pair[1]})^2"
+        else:
+            return f"(-V_{self.pair[0]} - V_{self.pair[1]})^2"
+
+    def __eq__(self, other: 'SecondaryVonorm'):
+        return self.pair == other.pair
+
+class VoronoiVector():
+
+    def __init__(self, vector_idx: int):
+        if vector_idx not in range(0, 4):
+            raise ValueError(f"VoronoiVector got invalid index: {vector_idx}")
+        self.idx = vector_idx
+
+    def dot(self, other: 'VoronoiVector'):
+        if self != other:
+            return Conorm(VoronoiVectorPair(self.idx, other.idx))
+        else:
+            return PrimaryVonorm(self.idx)
---
+++ tests/lattice/voronoi/test_voronoi_values.py
@@ -0,0 +1,41 @@
+import pytest
+
+from cnf.lattice.voronoi.voronoi_values import PrimaryVonorm, SecondaryVonorm, Conorm, VoronoiValue, VoronoiVector
+from cnf.lattice.voronoi.vector_pair import VoronoiVectorPair
+
+def test_can_instantiate_conorm():
+    c = Conorm((1, 2))
+    with pytest.raises(ValueError):
+        Conorm((1, 1))
+    assert Conorm((1, 2)) == Conorm((2, 1))
+
+
+def test_can_instantiate_primary_vonorm():
+    v = PrimaryVonorm(1)
+    assert v == PrimaryVonorm(1)
+    assert PrimaryVonorm(2) != PrimaryVonorm(1)
+    with pytest.raises(ValueError):
+        PrimaryVonorm(5)
+
+def test_can_instantiate_secondary_vonorm():
+    v = SecondaryVonorm((0, 1))
+    assert v.is_canonical
+    assert v.complement == SecondaryVonorm((3, 2))
+    assert not v.complement.is_canonical
+
+    with pytest.raises(ValueError):
+        SecondaryVonorm((2,2))
+
+    with pytest.raises(ValueError):
+        SecondaryVonorm((2,4))
+
+def test_can_dot_voronoi_vectors():
+    # Conorms
+    v1 = VoronoiVector(1)
+    v2 = VoronoiVector(2)
+    assert v1.dot(v2) == Conorm((1, 2))
+    assert v2.dot(v1) == Conorm((1, 2))
+    assert v2.dot(v1) != Conorm((3, 2))
+
+    assert v1.dot(v1) == PrimaryVonorm(1)
+    assert v1.dot(v1) != PrimaryVonorm(2)
