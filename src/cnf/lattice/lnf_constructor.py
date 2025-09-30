4b8f7d228239897dadb1c027dc656387cbe962f5
Max Gallant <mcgallant72@gmail.com>
2025-09-29 21:16:52 -0700

    Use sorted vonorm list in stabilizer perm construction

--- src/cnf/lattice/lnf_constructor.py
+++ src/cnf/lattice/lnf_constructor.py
@@ -35,7 +35,8 @@ class VonormCanonicalizer():
 
         sorted_vlists = sorted(permuted_vonorm_lists, key=lambda group: group[0].vonorms, reverse=False)
         canonical_vonorm_list = sorted_vlists[0][0]
-        stabilizer_permutations = [group[1] for group in permuted_vonorm_lists if group[0] == canonical_vonorm_list]
+        stabilizer_permutations = [group[1] for group in sorted_vlists if group[0] == canonical_vonorm_list]
+
         return CanonicalizedVonormResult(
             canonical_vonorm_list,
             reduction_result.transform_matrix,
