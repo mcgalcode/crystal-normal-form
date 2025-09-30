685b837036f02416826f51c20b94e9302181aa35
Max Gallant <mcgallant72@gmail.com>
2025-09-30 16:45:19 -0700

    Add fixture for loading some random MP cifs as test data

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,10 @@
 import pytest
+import os
 
 import numpy as np
+from pathlib import Path
+from pymatgen.core.structure import Structure
+
 
 @pytest.fixture(scope='package')
 def zr_bcc_primitive_lattice_vecs():
@@ -26,3 +30,11 @@ def zr_fcc_primitive_lattice_vecs():
         [1, 0, 1]
     ]) * zr_lattice_param / 2
     return fcc_primitive_vecs
+
+@pytest.fixture(scope='package')
+def mp_structures():
+    cif_dir = Path(__file__).parent / "data" / "mp_cifs"
+    cifs = []
+    for cif_path in cif_dir.iterdir():
+        cifs.append(Structure.from_file(cif_path))
+    return cifs
