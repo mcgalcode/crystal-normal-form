2623f61434d96423fdd51946ed887ab47c141ab4
Max Gallant <mcgallant72@gmail.com>
2025-09-30 19:03:17 -0700

    Add helper constructor for LNF

--- src/cnf/lattice/lattice_normal_form.py
+++ src/cnf/lattice/lattice_normal_form.py
@@ -2,6 +2,11 @@ from .voronoi.vonorm_list import VonormList
 
 class LatticeNormalForm():
 
+    @classmethod
+    def from_coords(cls, coords, lattice_step_size):
+        vlist = VonormList(coords)
+        return cls(vlist, lattice_step_size)
+
     def __init__(self, canonical_vonorm_list: VonormList, lattice_step_size: float):
         self.vonorms = canonical_vonorm_list
         self.lattice_step_size = lattice_step_size
