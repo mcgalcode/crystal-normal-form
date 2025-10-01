7d7e25f40588c0a7010a3790e77481144a457c59
Max Gallant <mcgallant72@gmail.com>
2025-09-30 19:04:14 -0700

    Add test for CNF equivalence

--- tests/test_crystal_normal_form.py
+++ tests/test_crystal_normal_form.py
@@ -11,4 +11,14 @@ def test_crystal_normal_form():
 
     lattice_vecs = Lattice.hexagonal(a=3.19, c=1.6*3.19)
     cnf = CrystalNormalForm.from_motif_and_basis_vecs(motif, lattice_vecs.matrix, motif_step=30)
-    print(cnf)
+    assert cnf is not None
+
+def test_crystal_normal_form_equivalence(mp_structures):
+    cnf_1 = CrystalNormalForm.from_pymatgen_structure(mp_structures[0])
+    cnf_1_1 = CrystalNormalForm.from_pymatgen_structure(mp_structures[0])
+    assert cnf_1 == cnf_1_1
+    assert len(set([cnf_1, cnf_1_1])) == 1
+
+    cnf_2 = CrystalNormalForm.from_pymatgen_structure(mp_structures[1])
+    assert cnf_2 != cnf_1
+    assert len(set([cnf_1, cnf_2])) == 2
