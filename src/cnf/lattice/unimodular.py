c5c56bf0fee78aa825d79a9e5515133d5c30530c
Max Gallant <mcgallant72@gmail.com>
2025-10-09 13:16:49 -0700

    Add helper for combining unimodular matrices

--- src/cnf/lattice/unimodular.py
+++ src/cnf/lattice/unimodular.py
@@ -7,3 +7,9 @@ def is_unimodular_set_closed(unimodular_mats):
     for m1 in unimodular_mats:
         for m2 in unimodular_mats:
             assert MatrixTuple(m1 @ m2).tuple in mat_tuples
+
+def combine_unimodular_matrices(unimodular_mats):
+    combined_mat = unimodular_mats[0]
+    for mat in unimodular_mats[1:]:
+        combined_mat = combined_mat @ mat
+    return combined_mat
