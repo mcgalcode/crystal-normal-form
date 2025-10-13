4173ccb2c8e639a58a5023edddd58422953397a2
Max Gallant <mcgallant72@gmail.com>
2025-10-13 12:13:08 -0700

    Add helper for constructing test data file paths

--- tests/helpers/data.py
+++ tests/helpers/data.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 from pymatgen.core.structure import Structure
 
+def get_data_file_path(path_in_data_dir):
+    return Path(__file__).parent / ".." / "data" / path_in_data_dir
+
 all_mp_cif_dir = Path(__file__).parent / ".." / "data" / "mp_cifs"
 ALL_MP_STRUCTURES = []
 for cif_path in all_mp_cif_dir.iterdir():
