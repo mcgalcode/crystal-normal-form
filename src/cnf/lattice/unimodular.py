4a97378ec5231801895b7668eb24a89bb0f73fba
Max Gallant <mcgallant72@gmail.com>
2025-10-13 17:09:46 -0700

    Fix bug in unimodular

--- src/cnf/lattice/unimodular.py
+++ src/cnf/lattice/unimodular.py
@@ -1,5 +1,5 @@
 from ..linalg import MatrixTuple
-
+import numpy as np
 def is_unimodular_set_closed(unimodular_mats):
     mat_tuples = set([MatrixTuple(m).tuple for m in unimodular_mats])
     assert len(mat_tuples) == len(unimodular_mats)
@@ -9,7 +9,7 @@ def is_unimodular_set_closed(unimodular_mats):
             assert MatrixTuple(m1 @ m2).tuple in mat_tuples
 
 def combine_unimodular_matrices(unimodular_mats):
-    combined_mat = unimodular_mats[0]
-    for mat in unimodular_mats[1:]:
+    combined_mat = MatrixTuple(np.eye(3))
+    for mat in unimodular_mats:
         combined_mat = combined_mat @ mat
     return combined_mat
