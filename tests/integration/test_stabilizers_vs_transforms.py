c134062d7bacecbaa84fc578291d9709b4600de1
Max Gallant <mcgallant72@gmail.com>
2025-10-14 12:11:17 -0700

    Make stabilizer test more thorough

--- tests/integration/test_stabilizers_vs_transforms.py
+++ tests/integration/test_stabilizers_vs_transforms.py
@@ -9,22 +9,25 @@ from cnf.linalg import MatrixTuple
 @helpers.parameterized_by_mp_structs
 def test_stabilizers_form_groups(idx, struct):
     uc = UnitCell.from_pymatgen_structure(struct).reduce()
+    eye = (0,1,2,3,4,5,6)
+    for p in uc.conorms.permissible_permutations:
+        vonorms = uc.vonorms.apply_permutation(p.vonorm_permutation)
+        vo_stab = [p.vonorm_permutation for p in vonorms.stabilizer_perms(tol=1e-4)]
+        assert is_permutation_set_closed(vo_stab)
+        assert eye in vo_stab
 
-    vonorms = uc.vonorms
-    vo_stab = [p.vonorm_permutation for p in vonorms.stabilizer_perms(tol=1e-4)]
-    assert is_permutation_set_closed(vo_stab)
+        co_stab = [p.conorm_permutation for p in vonorms.stabilizer_perms(tol=1e-4)]
+        assert is_permutation_set_closed(co_stab)
+        assert eye in co_stab
 
-    co_stab = [p.conorm_permutation for p in vonorms.stabilizer_perms(tol=1e-4)]
-    assert is_permutation_set_closed(co_stab)
-
-    mats = vonorms.stabilizer_matrices(tol=1e-4)
-    for mat1 in mats:
-        for mat2 in mats:
-            # assert mat1 @ MatrixTuple(np.random.randint(1,3, size=(3,3))) in mats
-            assert mat1 @ mat2 in mats
-    
-    for mat in mats:
-        assert mat.inverse() in mats
+        mats = vonorms.stabilizer_matrices(tol=1e-4)
+        for mat1 in mats:
+            for mat2 in mats:
+                # assert mat1 @ MatrixTuple(np.random.randint(1,3, size=(3,3))) in mats
+                assert mat1 @ mat2 in mats
+        
+        for mat in mats:
+            assert mat.inverse() in mats
 
 def test_transforms_for_sort_equivalent_to_stabilizer():
     fail_structs = []
