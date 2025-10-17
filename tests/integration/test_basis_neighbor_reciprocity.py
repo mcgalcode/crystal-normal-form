1d989bbb1d5823978d5c0d2d859c3f01060c8371
Max Gallant <mcgallant72@gmail.com>
2025-10-17 12:41:38 -0700

    Add integration tests for BNF neighbors

---
+++ tests/integration/test_basis_neighbor_reciprocity.py
@@ -0,0 +1,61 @@
+import pytest
+import numpy as np
+import helpers
+
+from cnf import CrystalNormalForm
+from cnf.cnf_constructor import CNFConstructor
+from cnf.navigation.basis_neighbor_finder import BasisStepResult, BasisNeighborFinder
+from cnf.navigation.neighbor import Neighbor
+from pymatgen.core.structure import Structure
+from cnf.unit_cell import UnitCell
+
+from pathlib import Path
+
+STRUCT_SAMPLE_FREQ = 1
+
+@helpers.skip_if_fast
+@helpers.parameterized_by_mp_structs
+def test_basis_neighbor_reciprocity(idx, struct: Structure):
+    struct = struct.to_primitive()
+    if len(struct) > 8:
+        return
+    verbose = False
+    xi = 1.5
+    delta = 10
+
+    helpers.printif("", verbose)
+    helpers.printif(f"Attempting struct at idx {idx * STRUCT_SAMPLE_FREQ}", verbose)
+    constructor = CNFConstructor(xi, delta, False) 
+
+    struct = struct.to_primitive()
+    
+    original_cnf = constructor.from_pymatgen_structure(struct).cnf
+
+    neighbor_set = BasisNeighborFinder(original_cnf).find_basis_neighbors()
+    print(f"Structure had {len(struct)} sites")
+    print(f"Found {len(neighbor_set)} basis neighbors!")
+    recipricol_nbs = [] 
+    nonreciprocal_nbs = []
+    geo_rec_neighbs = []
+    geo_matches = []
+    for nb_idx, n in enumerate(neighbor_set.neighbors):
+        assert n.point.lattice_normal_form == original_cnf.lattice_normal_form
+        second_neighbors = BasisNeighborFinder(n.point).find_basis_neighbors()
+        print(f"Found {len(neighbor_set)} basis SECOND neighbors!")
+        if original_cnf not in second_neighbors:
+            # helpers.save_cnfs_to_dir(f"patho_neighbor_pairs/mp_{idx}_nb_{nb_idx}", [original_cnf, n.point])
+            nonreciprocal_nbs.append(n.point)
+            num_geo_matches = 0
+
+            for n2 in second_neighbors.neighbors:
+                if helpers.are_cnfs_geo_matches(original_cnf, n2.point, tol=1e-7):
+                    num_geo_matches += 1
+                    geo_matches.append(n2.point)
+            if num_geo_matches > 0:
+                geo_rec_neighbs.append(n)
+            
+            helpers.printif(f"Found {num_geo_matches} geometrically identical second degree neighbs with same LNF!", verbose)
+        else:
+            recipricol_nbs.append(n)
+    
+    assert len(recipricol_nbs) == len(neighbor_set.neighbors)
---
+++ tests/integration/test_basis_neighbors_distinct.py
@@ -0,0 +1,68 @@
+import pytest
+import numpy as np
+import helpers
+
+from cnf import CrystalNormalForm
+from cnf.cnf_constructor import CNFConstructor
+from cnf.lattice.lnf_constructor import VonormCanonicalizer, LatticeNormalForm
+from cnf.navigation.basis_neighbor_finder import BasisStepResult, BasisNeighborFinder
+from pymatgen.core.structure import Structure
+from cnf.unit_cell import UnitCell
+
+
+@helpers.parameterized_by_mp_structs
+def test_neighbors_are_geometrically_distinct(idx, struct: Structure):
+    struct = struct.to_primitive()
+    if len(struct) > 10:
+        return    
+    verbose = False
+    xi = 1.5
+    delta = 20
+    constructor = CNFConstructor(xi, delta, False)
+    original_cnf = constructor.from_pymatgen_structure(struct).cnf
+    nf = BasisNeighborFinder(original_cnf)
+    neighb_set = nf.find_basis_neighbors()
+    tested_neighbs: list[CrystalNormalForm] = []
+    dups = []
+
+    clusters = {}
+
+    helpers.printif(f"Original structure is Voronoi: {original_cnf.lattice_normal_form.vonorms.conorms.form.voronoi_class}", verbose)
+    for neighb in neighb_set.neighbors:
+        steps = neighb_set.steps_for_neighbor(neighb.point)
+        current_cnf = neighb.point
+        is_duplicate = False
+        dup = None
+
+        for existing_cnf in clusters:
+            pdd_dist = helpers.pdd_for_cnfs(current_cnf, existing_cnf)
+            match, reason = helpers.are_cnfs_geo_matches(current_cnf, existing_cnf, tol=1e-12)
+            if match:
+                is_duplicate = True
+                verbose = False
+                print(existing_cnf.basis_normal_form.coord_list)
+                print(current_cnf.basis_normal_form.coord_list)
+                helpers.printif(f"This neighbor was reached by {len(steps)} steps", verbose)
+                clusters[existing_cnf].append(current_cnf)
+                dups.append((current_cnf, dup, pdd_dist))
+                helpers.printif("", verbose)
+
+        if not is_duplicate:
+            clusters[current_cnf] = [current_cnf]
+            tested_neighbs.append(neighb)
+
+    
+    print()
+    if not len(clusters) == len(neighb_set):
+        cidx = 0
+        for _, identical_neighbs in clusters.items():
+            if len(identical_neighbs) > 1:
+                print(f"{cidx} Cluster")
+                for nb in identical_neighbs:
+                    print(nb.basis_normal_form)
+                helpers.save_cnfs_to_dir(f"geo_pairs_basis_neighbs/mp_{idx}/cluster_{cidx}", identical_neighbs)
+                helpers.save_cifs_to_dir(f"geo_pairs_basis_neighbs_cifs/mp_{idx}/cluster_{cidx}", [c.reconstruct() for c in identical_neighbs])
+            cidx += 1
+    
+    assert len(clusters) == len(neighb_set)
+    # assert len(clusters) == chiral_neighbs
