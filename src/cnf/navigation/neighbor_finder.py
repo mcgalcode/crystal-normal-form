3c8e86b2c19b614c4bbdd82d6bee4879955ce97b
Max Gallant <mcgallant72@gmail.com>
2025-10-20 16:57:24 -0700

    Add neighbor finder class

---
+++ src/cnf/navigation/neighbor_finder.py
@@ -0,0 +1,26 @@
+from .lattice_neighbor_finder import LatticeNeighborFinder
+from .motif_neighbor_finder import MotifNeighborFinder
+from ..crystal_normal_form import CrystalNormalForm
+
+class NeighborFinder():
+
+    def __init__(self, point: CrystalNormalForm):
+        self.point = point
+
+    def find_neighbors(self):
+        lnf_neighbor_finder = LatticeNeighborFinder(self.point)
+        mnf_neighbor_finder = MotifNeighborFinder(self.point)
+
+        lattice_neighbors = lnf_neighbor_finder.find_cnf_neighbors()
+        mnf_neighbors = mnf_neighbor_finder.find_motif_neighbors()
+
+        
+        all_neighbor_points = set()
+        for lat_neighb in lattice_neighbors.neighbors:
+            all_neighbor_points.add(lat_neighb.point)
+        
+        for mot_neighb in mnf_neighbors.neighbors:
+            all_neighbor_points.add(mot_neighb.point)
+        
+        return all_neighbor_points
+        
