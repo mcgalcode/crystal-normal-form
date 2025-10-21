88b0b7cf955eb8ddcc66d123214213199a12fc81
Max Gallant <mcgallant72@gmail.com>
2025-10-20 18:22:26 -0700

    Add initial CrystalMap class for organizing graph structure

---
+++ src/cnf/navigation/crystal_map.py
@@ -0,0 +1,132 @@
+from ..crystal_normal_form import CrystalNormalForm
+from .neighbor_finder import NeighborFinder
+
+import rustworkx as rwx
+
+class CrystalMap():
+
+    @classmethod
+    def from_cnf(cls, cnf: CrystalNormalForm):
+        crys_map = cls(cnf.xi, cnf.delta, cnf.elements)
+        crys_map.add_point(cnf)
+        return crys_map
+
+    def __init__(self, xi: float, delta: int, element_list: list[str]):
+        self.xi = xi
+        self.delta = delta
+        self.element_list = element_list
+
+        self._graph = rwx.PyGraph()
+        self._all_points_set = set()
+        self._id_lookup = {}
+        self._is_explored = {}
+
+    def validate_point(self, cnf: CrystalNormalForm):
+        if not isinstance(cnf, CrystalNormalForm):
+            raise ValueError(f"Can't use point of type {type(cnf)} with CrystalMap - must use CrystalNormalForm instance.")
+
+        if cnf.xi != self.xi:
+            raise ValueError(f"Tried to add CNF with incompatible xi to CrystalMap (incoming: {cnf.xi}, map: {self.xi})")
+        
+        if cnf.delta != self.delta:
+            raise ValueError(f"Tried to add CNF with incompatible delta to CrystalMap (incoming: {cnf.delta}, map: {self.delta})")
+
+        if tuple(cnf.elements) != tuple(self.element_list):
+            raise ValueError(f"Tried to add CNF with incompatible element list to CrystalMap (incoming: {cnf.elements}, map: {self.element_list})")
+
+    def add_point(self, point: CrystalNormalForm):
+        self.validate_point(point)
+        if point in set(self.all_points()):
+            return None
+        node_id = self._graph.add_node(point)
+        self._all_points_set.add(point)
+        self._id_lookup[point] = node_id
+        self._is_explored[node_id] = False
+        return node_id
+    
+    def all_points(self):
+        return self._graph.nodes()
+    
+    def all_node_ids(self):
+        return list(self._graph.node_indices())
+    
+    def get_point_id(self, point: CrystalNormalForm):
+        return self._id_lookup.get(point)
+    
+    def get_point_ids(self, *points: list[CrystalNormalForm]):
+        return tuple([self.get_point_id(p) for p in points])
+
+    def get_point_by_id(self, id: int):
+        return self._graph[id]
+    
+    def remove_point(self, point: CrystalNormalForm):
+        if point not in self:
+            return None
+        point_id = self.get_point_id(point)
+        self._graph.remove_node(point_id)
+        self._all_points_set.remove(point)
+        del self._id_lookup[point]
+        del self._is_explored[point_id]
+        return point_id
+    
+    def is_point_explored(self, point: CrystalNormalForm):
+        pid = self.get_point_id(point)
+        return self.is_id_explored(pid)
+    
+    def is_id_explored(self, id: int):
+        if id not in self.all_node_ids():
+            raise ValueError(f"Tried to check if nonexistant node id {id} was explored")
+        return self._is_explored[id]
+    
+    def add_connection(self, pt1: CrystalNormalForm, pt2: CrystalNormalForm):
+        id1, id2 = self.get_point_ids(pt1, pt2)
+        return self.add_connection_by_ids(id1, id2)
+
+    def add_connection_by_ids(self, id1, id2):
+        if self._graph.has_edge(id1, id2):
+            return False
+        self._graph.add_edge(id1, id2, 1)
+        return True
+        
+    def remove_connection(self, pt1: CrystalNormalForm, pt2: CrystalNormalForm):
+        if not self.connection_exists(pt1, pt2):
+            return False
+        id1, id2 = self.get_point_ids(pt1, pt2)
+        self._graph.remove_edge(id1, id2)
+        return True
+
+    def connection_exists(self, pt1: CrystalNormalForm, pt2: CrystalNormalForm):
+        if pt1 not in self or pt2 not in self:
+            raise ValueError(f"Tried to look for connections involving a nonexistent point!")
+        
+        id1, id2 = self.get_point_ids(pt1, pt2)
+        return self.connection_exists_by_id(id1, id2)
+    
+    def connection_exists_by_id(self, id1: int, id2: int):
+        return self._graph.has_edge(id1, id2)
+    
+    @property
+    def unexplored_points(self) -> list[int]:
+        return list(self._is_explored.keys())
+    
+    def explore_point(self, point_id: int):
+        pt = self.get_point_by_id(point_id)
+        nf = NeighborFinder(pt)
+        nb_pts = nf.find_neighbors()
+        new_ids = []
+        for nb_pt in nb_pts:
+            if nb_pt not in self:
+                nid = self.add_point(nb_pt)
+                new_ids.append(nid)
+            self.add_connection(pt, nb_pt)
+        self._is_explored[point_id] = True
+        return new_ids
+    
+    def __contains__(self, item):
+        if not isinstance(item, CrystalNormalForm):
+            raise ValueError(f"Can't check if item {type(item)} is in CrystalMap - must use CrystalNormalForm instance.")
+        
+        return item in self._all_points_set
+    
+    def __len__(self):
+        return len(self._all_points_set)
