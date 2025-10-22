2a97cdb726b4313c1d687561ee1dfbf9efd8e9bc
Max Gallant <mcgallant72@gmail.com>
2025-10-22 10:58:25 -0700

    rename file for crystal explorer

--- tests/unit/navigation/test_crystal_map.py
+++ tests/unit/navigation/test_crystal_map.py
@@ -1,9 +1,10 @@
 import helpers
 import pytest
+import json
 from cnf import CrystalNormalForm, UnitCell
 from cnf.navigation.crystal_map import CrystalMap
 from cnf.navigation.neighbor_finder import NeighborFinder
-from cnf.navigation.exploration import get_endpoints_from_structs, CrystalExplorer
+from cnf.navigation.crystal_explorer import get_endpoints_from_structs, CrystalExplorer
 from tqdm import tqdm
 import time
 @pytest.fixture(scope='module')
