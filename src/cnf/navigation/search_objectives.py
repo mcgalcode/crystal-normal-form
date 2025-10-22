0b52f6f9b6e1ce496012bb564929a041a83a84e8
Max Gallant <mcgallant72@gmail.com>
2025-10-22 12:50:54 -0700

    Update locate any search objective

--- src/cnf/navigation/search_objectives.py
+++ src/cnf/navigation/search_objectives.py
@@ -1,22 +1,22 @@
 from abc import ABC, abstractmethod
 
-from typing import TYPE_CHECKING
 from ..crystal_normal_form import CrystalNormalForm
 
-if TYPE_CHECKING:
-    from .crystal_explorer import CrystalExplorer
 
 class SearchObjective(ABC):
 
     @abstractmethod
-    def objective_complete(self, explorer: CrystalExplorer) -> bool:
+    def objective_complete(self, explorer) -> bool:
         pass
 
 class LocateAnyTargetStruct(SearchObjective):
 
     def __init__(self, target_cnfs: list[CrystalNormalForm]):
         self.target_cnfs = target_cnfs
+        self.located_endpt = None
     
-    def objective_complete(self, explorer: CrystalExplorer):
-        return any([explorer.map.get_point_id(t) is not None for t in self.target_cnfs])
-        
+    def objective_complete(self, explorer):
+        for cnf in self.target_cnfs:
+            if cnf in explorer.map:
+                self.located_endpt = cnf       
+                return True
