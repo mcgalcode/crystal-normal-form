bef331c0c956c0674a6027124e80f2c666e4b889
Max Gallant <mcgallant72@gmail.com>
2025-11-11 11:16:38 -0800

    rename store

---
+++ src/cnf/db/cnf_store.py
@@ -0,0 +1,40 @@
+import sqlite3
+from . import queries
+
+
+### Database design notes:
+#
+# Table 1: point
+# id: int, primary key
+# cnf_str: JSON list (the actual CNF coordinate string)
+# value: float (the energy value for pathfinding, or e.g. a distance metric)
+# external_id: nullable, str (a field used to point to e.g. fireworks db entry for the calculation)
+# explored: bool
+
+#
+# Table 2: edge
+# source_id: int
+# target_id: int
+
+POINT_TABLE_NAME = 'point'
+
+class CNFStore():
+
+    @classmethod
+    def setup(cls, dbfname: str):
+        conn = sqlite3.connect(dbfname)
+        cur = conn.cursor()
+        cur.execute(queries.create_point_table)
+        conn.commit()
+        return cls(dbfname)
+
+
+    def __init__(self, dbfname: str):
+        self.db_name = dbfname
+        self.conn = sqlite3.connect(self.db_name)
+        self.cursor = self.conn.cursor()
+
+        query = queries.table_exists.format(table_name=POINT_TABLE_NAME)
+        res = self.cursor.execute(query)
+        if res.fetchone() is None:
+            raise ValueError(f"Tried to instantiate campaign store from uninitialized DB file: {dbfname}")
--- tests/unit/db/test_cnf_store.py
+++ tests/unit/db/test_cnf_store.py
@@ -1,20 +1,20 @@
 import pytest
-from cnf.db import CampaignStore
+from cnf.db.cnf_store import CNFStore
 import tempfile
 
 def test_cannot_instantiate_with_bad_db():
     with tempfile.NamedTemporaryFile('w') as tf:
         fname = tf.name
         with pytest.raises(ValueError) as captured_excep:
-            CampaignStore(fname)
+            CNFStore(fname)
         
         assert "Tried to instantiate campaign store from uninitialized DB file:" in captured_excep.value.__repr__()
 
 def test_can_instantiate_after_setup():
     with tempfile.NamedTemporaryFile('w') as tf:
 
-        cs = CampaignStore.setup(tf.name)
+        cs = CNFStore.setup(tf.name)
         assert cs is not None
 
-        cs2 = CampaignStore(tf.name)
+        cs2 = CNFStore(tf.name)
         assert cs2 is not None
