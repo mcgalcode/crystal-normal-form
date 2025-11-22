7df768a9fa43bdd34878f182e7d0f23ded7ed8f4
Max Gallant <mcgallant72@gmail.com>
2025-11-21 16:27:17 -0800

    Add relaxation helper to the grace calculator

--- src/cnf/calculation/grace.py
+++ src/cnf/calculation/grace.py
@@ -1,5 +1,6 @@
 from tensorpotential.calculator.foundation_models import grace_fm, GRACEModels
 from ..crystal_normal_form import CrystalNormalForm
+from ..navigation.neighbor_finder import NeighborFinder
 from ase import Atoms
 from .base_calculator import BaseCalculator
 
@@ -15,4 +16,23 @@ class GraceCalculator(BaseCalculator):
         atoms = cnf.reconstruct().to_ase_atoms()
         self._calc.calculate(atoms, properties = ['energy'])
         return self._calc.results['energy']
+    
+    def relax(self, cnf: CrystalNormalForm):
+        min_e = self.calculate_energy(cnf)
+        min_cnf = cnf
+        num_iters = 0
+        while True:
+            print(f"Energy: {min_e}")
+            nbs = NeighborFinder(min_cnf).find_neighbors()
+            nb_es = sorted([(self.calculate_energy(n), n) for n in nbs])
+            min_nb_e, min_nb = nb_es[0]
+            if min_nb_e < min_e:
+                min_e = min_nb_e
+                min_cnf = min_nb
+            else:
+                break
+            num_iters += 1
+        return min_cnf, min_e, num_iters
+
+            
     
--- tests/unit/calculation/test_grace.py
+++ tests/unit/calculation/test_grace.py
@@ -6,3 +6,10 @@ def test_can_load_calc(zr_bcc_mp):
     e = GraceCalculator().calculate_energy(cnf)
     assert e is not None
     assert isinstance(e, float)
+
+def test_relax(zr_bcc_mp):
+    cnf = CrystalNormalForm.from_pmg_struct(zr_bcc_mp, xi=1.5, delta=10)
+    min_cnf, min_e, num_iters = GraceCalculator().relax(cnf)
+    assert min_cnf != cnf
+    assert min_e < GraceCalculator().calculate_energy(cnf)
+    assert num_iters > 0
