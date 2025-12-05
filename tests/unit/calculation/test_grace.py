6769b37aea83171760780351503ba50f5b9f6c23
Max Gallant <mcgallant72@gmail.com>
2025-12-05 14:53:49 -0800

    skip grace tests for now

--- tests/unit/calculation/test_grace.py
+++ tests/unit/calculation/test_grace.py
@@ -1,12 +1,15 @@
+import pytest
 from cnf.calculation.grace import GraceCalculator
 from cnf import CrystalNormalForm
 
+@pytest.mark.skip
 def test_can_load_calc(zr_bcc_mp):
     cnf = CrystalNormalForm.from_pmg_struct(zr_bcc_mp, xi=1.5, delta=1)
     e = GraceCalculator().calculate_energy(cnf)
     assert e is not None
     assert isinstance(e, float)
 
+@pytest.mark.skip
 def test_relax(zr_bcc_mp):
     cnf = CrystalNormalForm.from_pmg_struct(zr_bcc_mp, xi=1.5, delta=10)
     min_cnf, min_e, num_iters = GraceCalculator().relax(cnf)
