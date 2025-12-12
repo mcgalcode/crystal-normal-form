d3f0163cd3af0101ad1abd6c5a96f623ec58f8fb
Max Gallant <mcgallant72@gmail.com>
2025-12-11 21:27:29 -0800

    Add tuple route to motif neighbor finding

--- src/cnf/navigation/motif_neighbor_finder.py
+++ src/cnf/navigation/motif_neighbor_finder.py
@@ -78,12 +78,41 @@ class MotifNeighborFinder():
         mnf_constructor = MNFConstructor(self.point.delta, stabilizers)
         mnf_coords_list = mnf_constructor.build_many_from_raw_coords(coords_list, atomic_numbers, use_rust=USE_RUST)
 
-        # Build CNFs from MNFs
-        lnf = LatticeNormalForm(vonorms, self.point.xi)
+        # Return tuples with metadata
+        results = []
+        vonorms_tuple = vonorms.tuple
         for (nb_mnf, affected_idxs, adj), mnf_coords in zip(neighbor_mnf_tuples, mnf_coords_list):
-            # Create CNF directly
-            mnf = MotifNormalForm(mnf_coords, original_els, self.point.delta)
+            results.append((vonorms_tuple, mnf_coords, affected_idxs, adj))
+
+        return results
+
+    def tuples_to_motif_neighbors(self, neighbor_tuples):
+        """
+        Convert neighbor tuples to MotifStepResult objects.
+
+        Args:
+            neighbor_tuples: List of (vonorms_tuple, coords_tuple, affected_idxs, adj)
+
+        Returns:
+            NeighborSet containing MotifStepResult objects
+        """
+        nbs = NeighborSet()
+        original_els = self.point.motif_normal_form.elements
+        xi = self.point.xi
+        delta = self.point.delta
+
+        for vonorms_tuple, mnf_coords, affected_idxs, adj in neighbor_tuples:
+            # Create objects
+            vonorms = self.point.lattice_normal_form.vonorms  # Reuse same vonorms object
+            lnf = LatticeNormalForm(vonorms, xi)
+            mnf = MotifNormalForm(mnf_coords, original_els, delta)
             cnf = CrystalNormalForm(lnf, mnf)
             result = MotifStepResult(cnf, affected_idxs, adj)
             nbs.add_neighbor(result)
+
         return nbs
+
+    def find_motif_neighbors(self):
+        """Find motif neighbors."""
+        neighbor_tuples = self.find_neighbor_tuples()
+        return self.tuples_to_motif_neighbors(neighbor_tuples)
