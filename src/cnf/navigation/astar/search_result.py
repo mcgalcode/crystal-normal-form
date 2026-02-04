e9a16d47106fb3093b74ae622234d22eb7768f44
Max Gallant <mcgallant72@gmail.com>
2026-02-03 16:03:09 -0800

    Add saerch result class for storing astar pathfinding results

---
+++ src/cnf/navigation/astar/search_result.py
@@ -0,0 +1,37 @@
+"""A* pathfinding for CNF navigation with customizable heuristics and filters"""
+
+from typing import List, Optional, Set
+from dataclasses import dataclass, field
+from .node import AStarNode
+
+from cnf import CrystalNormalForm
+
+@dataclass
+class PathSearchResult:
+    """Complete state of an A* search, for saving/resuming or extracting frontier points"""
+
+    # Store CNF metadata for reconstructing CNF objects
+    xi: float = field()
+    delta: int = field()
+    elements: tuple = field()
+    n_atoms: int
+    num_iterations: int
+
+    greedy: bool
+    beam_width: Optional[int]
+    dropout: float
+    max_iterations: int
+
+    metadata: dict
+
+    path: Optional[list[list[int]]] = field(default=None) # The path if found, None otherwise
+    max_iterations_reached: bool = field(default=False)
+
+    def get_cnfs_on_path(self):
+        if self.path is None:
+            return None
+        else:
+            return [
+                CrystalNormalForm.from_tuple(pt, self.elements, self.xi, self.delta)
+                for pt in self.path
+            ]
