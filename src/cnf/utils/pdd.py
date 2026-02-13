51aab8df75f6c9a5a9d13716d6b083b46247bcb2
Max Gallant <mcgallant72@gmail.com>
2026-02-12 17:00:42 -0800

    add AMD utility function

--- src/cnf/utils/pdd.py
+++ src/cnf/utils/pdd.py
@@ -29,6 +29,20 @@ def pdd_amd(struct1: Structure, struct2: Structure, k=None):
 
     return amd.AMD_cdist([amd1], [amd2])[0][0]
 
+def amd_from_cnf(cnf: CrystalNormalForm, k=None):
+    struct = cnf.reconstruct()
+    amd_struct_1 = amd.periodicset_from_pymatgen_structure(struct)
+
+    # calculate PDDs
+    if k is None:
+        k = max(100, struct.num_sites * 3)
+
+    amd1 = amd.AMD(amd_struct_1, k)
+    return amd1
+
+
+
+
 def pdd_for_cnfs(cnf1: CrystalNormalForm, cnf2: CrystalNormalForm, k=100):
     return pdd(cnf1.reconstruct(), cnf2.reconstruct(), k=k)
 
