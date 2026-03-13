ec45975e86b4cd6c0f0cce7a685eff479a69ef82
Max Gallant <mcgallant72@gmail.com>
2026-03-13 16:24:01 -0700

    add quickstart guide

--- src/cnf/__init__.py
+++ src/cnf/__init__.py
@@ -1,4 +1,5 @@
 from .crystal_normal_form import CrystalNormalForm
+from .cnf_constructor import CNFConstructor
 from .lattice import LatticeNormalForm
 from .motif import MotifNormalForm
 from .unit_cell import UnitCell
