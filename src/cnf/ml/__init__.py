789b244c3ba7b4773b518b5f3eac1b22b99a85e9
Max Gallant <mcgallant72@gmail.com>
2026-03-13 15:47:52 -0700

    rm unused module

--- src/cnf/ml/__init__.py
+++
@@ -1,7 +0,0 @@
-# Active Learning Study
-# 
-# Goal: Test CNF exploration as method for generating configurations in an active learning setting:
-#
-# Test system: Cu
-# 1. Collect Cu polymorphs/energies (GGA) from MP
-# 
